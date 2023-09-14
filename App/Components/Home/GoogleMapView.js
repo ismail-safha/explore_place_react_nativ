8dd4f08a370666597e5de19406b148bcbb2731d8
ismail safha <ismailsafha@gmail.com>
2023-09-14 20:16:41 +0100

    Integrate Google Map

---
+++ App/Components/Home/GoogleMapView.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { View, Text, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+export default function GoogleMapView() {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0422,
+    longitudeDelta: 0.0421,
+  });
+  return (
+    <View style={{ marginTop: 20 }}>
+      <MapView
+        style={{
+          width: Dimensions.get("screen").width * 0.9,
+          height: Dimensions.get("screen").height * 0.23,
+          borderRadius: 20,
+        }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+      ></MapView>
+    </View>
+  );
+}
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1. add Navigation Tab
 
 2. create Header
+
+3. Google MAP api Key
+
+- Integrate Google Map
